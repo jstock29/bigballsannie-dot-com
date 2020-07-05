1468e15bf0220ae399d426db4f15e6ba3b07623b
Jared <jstock529@gmail.com>
2020-07-05 13:51:59 -0400

    Add entry dialog and drag and drop cdk

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,67 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Inject } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
+export interface DialogData {
+	answer: number;
+	problem: string;
+	solution: number;
+}
 
 @Component({
 	selector: 'app-root',
 	templateUrl: './app.component.html',
 	styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+	answer: number;
+	problem: string;
+	solution: number;
+
+	constructor(public dialog: MatDialog) { }
 	title = 'Big Balls Island';
 	welcome_text = 'This is Big Balls Island 🏝, the home of the notorious Big Balls Annie. BBA runs things around here, so you better grow some balls or leave fast.'
+
+	openDialog() {
+		this.answer = null;
+		this.problem = 'problem';
+		this.solution = 1.0
+
+		const dialogRef = this.dialog.open(EntryDialog, {
+			width: '99vw',
+			height: '99vh',
+			// disableClose: true,
+			// backdropClass: 'blackout-backdrop',
+			data: {
+				answer: this.answer,
+				problem: this.problem,
+				solution: this.solution
+			}
+		});
+
+		dialogRef.afterClosed().subscribe(result => {
+			console.log('The dialog was closed');
+			console.log(result);
+		});
+	}
+
+	ngOnInit() {
+		this.openDialog()
+	}
+
+}
+
+@Component({
+	selector: 'entry-dialog',
+	templateUrl: 'entry-dialog.html',
+})
+export class EntryDialog {
+	constructor(public dialogRef: MatDialogRef<EntryDialog>,
+		@Inject(MAT_DIALOG_DATA) public data: DialogData) { console.log(data) }
+
+	checkAnswer(answer, solution) {
+		console.log(answer, solution)
+		if (answer === solution) {
+			this.dialogRef.close()
+		}
+	}
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,22 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent, EntryDialog } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { MatGridListModule } from '@angular/material/grid-list';
 import { MatCardModule } from '@angular/material/card';
 import { MatMenuModule } from '@angular/material/menu';
 import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatDialogModule } from '@angular/material/dialog';
 import { MatButtonModule } from '@angular/material/button';
 import { LayoutModule } from '@angular/cdk/layout';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
+
 import { StocksComponent } from './stocks/stocks.component';
 import { BulletinComponent } from './bulletin/bulletin.component';
 import { TradesComponent } from './trades/trades.component';
@@ -22,6 +29,7 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 @NgModule({
 	declarations: [
 		AppComponent,
+		EntryDialog,
 		DashboardComponent,
 		StocksComponent,
 		BulletinComponent,
@@ -30,6 +38,7 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 	],
 	imports: [
 		BrowserModule,
+		FormsModule,
 		AppRoutingModule,
 		BrowserAnimationsModule,
 		MatGridListModule,
@@ -37,12 +46,16 @@ import { LeaderboardComponent } from './leaderboard/leaderboard.component';
 		MatMenuModule,
 		MatIconModule,
 		MatButtonModule,
+		MatDialogModule,
+		MatFormFieldModule,
+		MatInputModule,
 		LayoutModule,
 		MatTableModule,
 		MatPaginatorModule,
-		MatSortModule
+		MatSortModule,
+		DragDropModule
 	],
-	entryComponents: [StocksComponent, BulletinComponent],
+	entryComponents: [StocksComponent, BulletinComponent, TradesComponent, LeaderboardComponent, EntryDialog],
 	providers: [],
 	bootstrap: [AppComponent]
 })
