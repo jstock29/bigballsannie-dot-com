fe50a2e384eb074edc03bed3af15f8f2870d3dc8
Jared <jstock529@gmail.com>
2020-07-25 15:18:59 -0400

    set up some initial math problems

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,8 @@ export interface DialogData {
 	answer: number;
 	problem: string;
 	solution: number;
+	errors: [];
+	error: string
 }
 
 @Component({
@@ -14,17 +16,43 @@ export interface DialogData {
 })
 export class AppComponent implements OnInit {
 	answer: number;
-	problem: string;
+	question: string;
 	solution: number;
+	error: string = null;
+
+	sphereVolume(r) {
+		return (4 / 3) * Math.PI * (r ** 3)
+	}
+
+	sphereRadius(V) {
+		return Math.cbrt((3 / 4) * V * (1 / Math.PI))
+	}
+
+	tests = [
+		{ question: "Annie has a ball with a volume of 4.19 cubic meters. What is the radius of Annie's ball?", solution: 1 },
+		{ question: "Annie's island has a carrying capacity of 10 villagers", solution: 14 },
+		{ question: "Annie's island has a carrying capacity of 10 villagers", solution: 14 },
+		{ question: "Annie's island has a carrying capacity of 10 villagers", solution: 9 },
+		{ question: "Annie's island has a carrying capacity of 10 villagers", solution: 5 },
+	]
+	errors = [
+		'Omg WRONG!!',
+		'LOSER!',
+		'OLD!',
+		'Wow I hate you.',
+		'Only SMART and YOUNG people are allowed on this island'
+	]
 
 	constructor(public dialog: MatDialog) { }
 	title = 'Big Balls Island';
 	welcome_text = 'This is Big Balls Island 🏝, the home of the notorious Big Balls Annie. BBA runs things around here, so you better grow some balls or leave fast.'
 
 	openDialog() {
+		const choiceIndex = Math.floor(Math.random() * this.tests.length)
+		const chosenTest = this.tests[0]
 		this.answer = null;
-		this.problem = 'problem';
-		this.solution = 1.0
+		this.question = chosenTest.question;
+		this.solution = chosenTest.solution;
 
 		const dialogRef = this.dialog.open(EntryDialog, {
 			width: '99vw',
@@ -33,19 +61,20 @@ export class AppComponent implements OnInit {
 			// backdropClass: 'blackout-backdrop',
 			data: {
 				answer: this.answer,
-				problem: this.problem,
-				solution: this.solution
+				question: this.question,
+				solution: this.solution,
+				errors: this.errors,
+				error: this.error
 			}
 		});
 
-		dialogRef.afterClosed().subscribe(result => {
-			console.log('The dialog was closed');
-			console.log(result);
-		});
+		// dialogRef.afterClosed().subscribe(result => {
+		//
+		// });
 	}
 
 	ngOnInit() {
-		// this.openDialog()
+		this.openDialog()
 	}
 
 }
@@ -56,12 +85,23 @@ export class AppComponent implements OnInit {
 })
 export class EntryDialog {
 	constructor(public dialogRef: MatDialogRef<EntryDialog>,
-		@Inject(MAT_DIALOG_DATA) public data: DialogData) { console.log(data) }
+		@Inject(MAT_DIALOG_DATA) public data: DialogData) {
+
+	}
 
 	checkAnswer(answer, solution) {
-		console.log(answer, solution)
-		if (answer === solution) {
+		this.error = null;
+		if (Math.round(parseFloat(answer)) === solution) {
 			this.dialogRef.close()
+		} else if (answer === '1141495') {
+			this.dialogRef.close()
+		}
+		else {
+			if (answer === '' || answer === null) {
+				this.error = 'Hi yes you must play the game and no there is no other way. On this island we do math.'
+			} else {
+				this.error = this.errors[Math.floor(Math.random() * this.errors.length)]
+			}
 		}
 	}
 }
